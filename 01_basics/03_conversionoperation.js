4a7309ee4d79428afec248ef952129ba28ad6283
VAGISHA YADAV <167693353+VAGISHA-47@users.noreply.github.com>
2025-08-22 01:46:56 +0000

    conversion of datatypes

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -26,9 +26,61 @@ console.log(booleanIsLoggedIn);
 */
 //1 => true; 0 => false
 //"" => false; "vagisha" => true 
-
+/*
 let someNumber = 33
 
 let stringNumber = String(someNumber)
 console.log(stringNumber);
 console.log(typeof stringNumber);
+*/
+
+/////////////////////////////////////////////////////////////////
+
+//let value =3
+//let negValue = -value
+//console.log(negvalue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console,log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+let str1 ="tashu"
+let str2 ="yadav"
+let str3 = str1+ str2
+/*console.log(str3);
+console.log("1" +2);
+console.log(1 + 2);
+console.log(1+"2");
+console.group("1"+"2");
+console.log("1"+2+2);
+console.log(1+2+"2");
+console.log((3+4)*5 % 3);
+*/
+//console.log(+true);
+//console.log(+"");
+/*
+let num1 , num2 , num3
+num1=num2=num3=2+2 //wrong method
+
+let gameCounter= 100;
+//gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+*/
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+// Expected output: "x:4, y:3"
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
+// Expected output: "a:4, b:4"
+
+
